2b6178145d1c3d969e20f13f27bcd2c7cf55d413
DivanshuChoudhary <choudharydivanshu75@gmail.com>
2026-06-26 22:18:52 +0530

    usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
+import { useState } from "react";
 import "./index.css";
 
 function App() {
+  const [task, setTask] = useState("");
   return (
     <div className="container">
       <h1>📝 To-Do App</h1>
 
       <div className="input-box">
         <input
-          type="text"
-          placeholder="Enter your task..."
-        />
+  type="text"
+  placeholder="Enter your task..."
+  value={task}
+  onChange={(e) => setTask(e.target.value)}
+/>
 
         <button>Add Task</button>
       </div>
+
+      <p className="preview">Typing: {task}</p>
 
       <div className="todo-list">
         <p>No tasks yet.</p>
